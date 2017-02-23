0514a772f44823fe42440b69ceaafa82131ede8c
LQQZYY <592852848@qq.com>
2017-02-23 16:52:18 +0800

    no message

--- UIPageViewController-OC/UIPageViewController-OC/LZSegmentViewController/LDSegmentView/LDSegmentView.h
+++ UIPageViewController-OC/UIPageViewController-OC/LZSegmentViewController/LDSegmentView/LDSegmentView.h
@@ -32,3 +32,26 @@ typedef void(^segmentBlock)(NSInteger index);
 
 - (void)segmentView:(LDSegmentView *)view didSelectedIndex:(NSInteger)index ;
 @end
+
+
+///************************************************************************************************************************************************
+#pragma LDSegmentCell
+@interface LDSegmentCell : UICollectionViewCell
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, assign) BOOL isSelected;
+
+@property (nonatomic, strong) UIColor *selectedColor;
+@property (nonatomic, strong) UIColor *normalColor;
+@property (nonatomic, assign) CGFloat fontSize;
+@end
+
+// 类内部使用的Model
+@interface LDSegmentModel : NSObject
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) BOOL selected;
+
+@property (nonatomic, assign) CGFloat fontSize;
+@end
--- UIPageViewController-OC/UIPageViewController-OC/LZSegmentViewController/LDSegmentView/LDSegmentView.m
+++ UIPageViewController-OC/UIPageViewController-OC/LZSegmentViewController/LDSegmentView/LDSegmentView.m
@@ -7,7 +7,6 @@
 //
 
 #import "LDSegmentView.h"
-#import "LDSegmentCell.h"
 
 @interface LDSegmentView () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate>
 
@@ -216,11 +215,7 @@
     NSIndexPath *path = [NSIndexPath indexPathForRow:_selectedIndex inSection:0];
     [self moveLineToIndexPath:path animation:YES];
 }
-- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
-    
-    NSIndexPath *path = [NSIndexPath indexPathForRow:_selectedIndex inSection:0];
-    
-}
+
 #pragma mark - 移动横线
 - (void)moveLineToIndexPath:(NSIndexPath *)indexPath animation:(BOOL)animation {
     
@@ -267,4 +262,83 @@
 }
 */
 
+@end
+
+///************************************************************************************************************************************************
+#pragma LDSegmentCell
+static CGFloat __fontSize = 12.0;
+@interface LDSegmentCell ()
+
+@property (nonatomic, strong) UILabel *titleLabel;
+@end
+@implementation LDSegmentCell
+- (UILabel *)titleLabel {
+    if (_titleLabel == nil) {
+        _titleLabel = [[UILabel alloc]init];
+        _titleLabel.textAlignment = NSTextAlignmentCenter;
+        _titleLabel.font = [UIFont systemFontOfSize:12];
+        
+        [self addSubview:_titleLabel];
+    }
+    
+    return _titleLabel;
+}
+- (void)setTitle:(NSString *)title {
+    _title = title;
+    
+    self.titleLabel.text = title;
+}
+
+- (void)setIsSelected:(BOOL)isSelected {
+    _isSelected = isSelected;
+    
+    self.titleLabel.textColor = isSelected ? _selectedColor : _normalColor;
+}
+- (void)setFontSize:(CGFloat)fontSize {
+    _fontSize = fontSize;
+    
+    self.titleLabel.font = [UIFont systemFontOfSize:fontSize];
+    __fontSize = fontSize;
+}
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        _selectedColor = [UIColor redColor];
+        _normalColor = [UIColor whiteColor];
+        _fontSize = 12.0;
+    }
+    
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    self.titleLabel.backgroundColor = self.backgroundColor;
+    self.titleLabel.frame = self.bounds;
+}
+@end
+
+
+@implementation LDSegmentModel
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _selected = NO;
+        _fontSize = 12.0;
+    }
+    
+    return self;
+}
+
+- (CGFloat)width {
+    if (_width <= 0) {
+        CGFloat wid = [self.title boundingRectWithSize:CGSizeMake(0, 30) options:NSStringDrawingUsesFontLeading|NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:self.fontSize]} context:nil].size.width;
+        
+        _width = wid + 10;
+    }
+    
+    return _width;
+}
 @end
